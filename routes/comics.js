6a200010c8e7d763408d71ca8c7b21e6f2e8776b
Quentin Chapon <q.chapon@gmail.com>
2021-08-04 20:38:49 +0200

    Added comics route

--- routes/comics.js
+++ routes/comics.js
@@ -1,13 +1,18 @@
 const express = require("express");
+const axios = require("axios");
 const router = express();
 
 // Comics route
 
 router.get("/comics", async (req, res) => {
   try {
-    res.json("Ok");
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.MARVEL_API_KEY}`
+    );
+    console.log(response.data);
+    res.json(response.data);
   } catch (error) {
-    res.status(400).json({ message: "An error occured" });
+    console.log(error.message);
   }
 });
 
