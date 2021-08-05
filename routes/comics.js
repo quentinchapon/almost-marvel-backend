c58af6a929bbce52e7f2b2c562a463f5be29aca3
Quentin Chapon <q.chapon@gmail.com>
2021-08-05 11:13:14 +0200

    Added comics route for one character

--- routes/comics.js
+++ routes/comics.js
@@ -15,4 +15,18 @@ router.get("/comics", async (req, res) => {
   }
 });
 
+// Get all comics for one character
+
+router.get("/comics/character:id", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics/${req.query.itemDatas}?apiKey=${process.env.MARVEL_API_KEY}`
+    );
+    console.log(req.query.itemDatas);
+    res.json(response.data);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
 module.exports = router;
