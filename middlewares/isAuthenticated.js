252a7e4d112399a78fe0b7d093e41e77650b4d4a
Quentin Chapon <q.chapon@gmail.com>
2021-08-07 02:13:41 +0200

    Added Sign up route

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -1,22 +1,22 @@
-const User = require("../models/User");
+// const User = require("../models/User");
 
-const isAuthenticated = async (req, res, next) => {
-  try {
-    // Get client token
-    const token = req.headers.authorization.replace("Bearer ", "");
-    // Find user with token
-    const user = await User.findOne({ token: token }).select("account");
-    // If found (next)
-    if (user) {
-      req.user = user;
-      next();
-    } else {
-      // Else ==> "Unauthorized"
-      res.status(401).json({ message: "Unauthorized" });
-    }
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-};
+// const isAuthenticated = async (req, res, next) => {
+//   try {
+//     // Get client token
+//     const token = req.headers.authorization.replace("Bearer ", "");
+//     // Find user with token
+//     const user = await User.findOne({ token: token }).select("account");
+//     // If found (next)
+//     if (user) {
+//       req.user = user;
+//       next();
+//     } else {
+//       // Else ==> "Unauthorized"
+//       res.status(401).json({ message: "Unauthorized" });
+//     }
+//   } catch (error) {
+//     res.status(400).json({ message: error.message });
+//   }
+// };
 
-module.exports = isAuthenticated;
+// module.exports = isAuthenticated;
